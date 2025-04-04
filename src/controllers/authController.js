f159735e0a4d54832af37b91a4aad3c620a256d1
hashe_lee <Hasheleeanuththara@gmail.com>
2025-04-04 22:50:19 +0530

    change and forgot password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,44 +8,99 @@ dotenv.config();
 
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
-  const isUser = true;
-  await loginByEntity(isUser, email, password, res);
+  await loginByEntity(true, email, password, res);
 };
 
 export const loginPharmacy = async (req, res) => {
   const { email, password } = req.body;
-  const isUser = false;
-  await loginByEntity(isUser, email, password, res);
+  await loginByEntity(false, email, password, res);
 };
 
 export const changePassword = async (req, res) => {
- try {
-   const { oldPassword, newPassword } = req.body;
-   const userId = req.user.userId;
-   const email = req.user.email; 
-   const role = req.user.role;
- 
-   const Model = role === "user" ? User : Pharmacy;
- 
-   const user = await Model.findOne({_id: userId,email: email});
-   if(!user) return res.status(404).json({ message: "User not found" });
- 
-   const isMatch = await bcrypt.compare(oldPassword, user.password);
-   if (!isMatch)
-     return res.status(401).json({ message: "Invalid old password" });
- 
-   const hashedPassword = await bcrypt.hash(newPassword, 10);
- 
-   user.password = hashedPassword;
-   await user.save();
- 
-   res.status(200).json({ message: "Password changed successfully" });
- 
- } catch (error) {
-  console.error("Change Password Error:", error);
-  res.status(500).json({ message: "Internal Server Error" });
-  
- }
+  try {
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await validateUser(req, res);
+    if (!user) return;
+
+    if (!oldPassword || !newPassword) {
+      return res
+        .status(400).json({ message: "oldPassword and newPassword are required" });
+    }
+
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid old password" });
+
+    await saveNewPassword(newPassword, user, res);
+  } catch (error) {
+    console.error("Change Password Error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+    return null;
+  }
+};
+
+export const forgotPasswordUser = async (req, res) => {
+  forgotPassword(req, res, true);
+};
+
+export const forgotPasswordPharmacy = async (req, res) => {
+  forgotPassword(req, res, false);
+};
+
+const forgotPassword = async (req, res, isUser) => {
+  try {
+    const { email, newPassword } = req.body;
+
+    if (!email || !newPassword) {
+      return res.status(400).json({ message: "email is required" });
+    }
+
+    const Model = isUser ? User : Pharmacy;
+
+    const user = await Model.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    await saveNewPassword(newPassword, user, res);
+  } catch (error) {
+    console.error("Forgot Password Error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const validateUser = async (req, res) => {
+  try {
+    const { userId, email, role } = req.user;
+    const Model = role === "user" ? User : Pharmacy;
+
+    const user = await Model.findOne({ _id: userId, email });
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return null;
+    }
+
+    return user;
+  } catch (error) {
+    console.error("Validate User Error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+    return null;
+  }
+};
+
+const saveNewPassword = async (newPassword, user, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    user.password = hashedPassword;
+    await user.save();
+
+    res.status(200).json({ message: "Password changed successfully" });
+  } catch (error) {
+    console.error("Save new Password Error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 const generateToken = (userId, email, role) => {
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser,loginPharmacy,changePassword } from "../controllers/authController.js";
+import { loginUser,loginPharmacy,changePassword, forgotPasswordUser, forgotPasswordPharmacy } from "../controllers/authController.js";
 import { authenticateUser,authenticatePharmacy } from "../middleware/authMiddleware.js"; 
 const router = Router();
 
@@ -7,5 +7,7 @@ router.post("/user/login",loginUser);
 router.post("/pharmacy/login",loginPharmacy);
 router.post("/user/changePassword",authenticateUser,changePassword);
 router.post("/pharmacy/changePassword",authenticatePharmacy,changePassword);
+router.post("/user/forgotPassword",forgotPasswordUser);
+router.post("/pharmacy/forgotPassword",forgotPasswordPharmacy);
 
 export default router;
