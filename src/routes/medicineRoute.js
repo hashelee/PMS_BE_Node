cee7790820ed52a990e817a170c95fb6ceb1fa30
hashe_lee <Hasheleeanuththara@gmail.com>
2025-08-03 22:41:16 +0530

    add search medicine endpoint

--- src/routes/medicineRoute.js
+++ src/routes/medicineRoute.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { createMedicine, deleteMedicine, editMedicine, getMedicineById } from "../controllers/medicineController.js";
-import { authenticatePharmacy } from "../middleware/authMiddleware.js";
+import { createMedicine, deleteMedicine, editMedicine, getMedicineById, getPharmaciesByMedicine } from "../controllers/medicineController.js";
+import { authenticatePharmacy, authenticateUser } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.post("/create",authenticatePharmacy,createMedicine);
 router.patch("/:medicineId/edit",authenticatePharmacy,editMedicine);
 router.delete("/:medicineId/delete",authenticatePharmacy,deleteMedicine);
+
+router.get("/search", authenticateUser, getPharmaciesByMedicine);
 router.get("/:medicineId",authenticatePharmacy,getMedicineById);
 
 export default router;
