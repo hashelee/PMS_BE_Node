2bdf5b359fb0e12f1936e59402a4a14b3b1910ef
hashe_lee <Hasheleeanuththara@gmail.com>
2025-11-15 01:16:21 +0530

    update approve by pharmacy

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -13,15 +13,28 @@ export const createOrder = async (req, res) => {
       return res.status(400).json({ message: "Item list cannot be empty" });
     }
     for (const item of items) {
-        if (!item.medicine || !item.quantity || item.quantity < 1) {
-            return res.status(400).json({ message: "Each item must have a valid medicine ID and quantity greater than 0" });
-        }
+      if (!item.medicine || !item.quantity || item.quantity < 1) {
+        return res.status(400).json({
+          message:
+            "Each item must have a valid medicine ID and quantity greater than 0",
+        });
+      }
 
-        const medicineExists = await Medicine.findById(item.medicine);
-        if (!medicineExists) {
-            return res.status(400).json({ message: `Medicine with ID ${item.medicine} does not exist` });
-        }
+      const medicineExists = await Medicine.findById(item.medicine);
+      if (!medicineExists) {
+        return res.status(400).json({
+          message: `Medicine with ID ${item.medicine} does not exist`,
+        });
+      }
+      medicineExists.quantity -= item.quantity;
+      if (medicineExists.quantity < 0) {
+        return res.status(400).json({
+          message: `Insufficient stock for medicine ID ${item.medicine}`,
+        });
+      }
+      await medicineExists.save();
     }
+
     const newOrder = {
       userId: user._id,
       medicines: items,
@@ -41,17 +54,17 @@ export const createOrder = async (req, res) => {
 
 export const getUserOrders = async (req, res) => {
   const { userId, email, role } = req.user;
-    try {
-        const user = await validateUser(userId, email, role);
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-        const orders = await Order.find({ userId: user._id }).populate('medicines.medicine').sort({ createdAt: -1 });
-        res.status(200).json({ orders });
-    } catch (error) {
-        console.error("Error fetching user orders:", error);
-        return res.status(500).json({ message: "Internal server error" });
+  try {
+    const user = await validateUser(userId, email, role);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+    const orders = await Order.find({ userId: user._id })
+      .populate("medicines.medicine")
+      .sort({ createdAt: -1 });
+    res.status(200).json({ orders });
+  } catch (error) {
+    console.error("Error fetching user orders:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
-
-ex
